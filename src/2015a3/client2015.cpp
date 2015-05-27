37b7e4414a3b0a1ff3dc7cf7029eb97f8bdf6e45
Mooophy <moophyalan@gmail.com>
2015-05-27 22:46:15 +1200

    modified:   src/2015a3/client2015.cpp

--- src/2015a3/client2015.cpp
+++ src/2015a3/client2015.cpp
@@ -11,38 +11,37 @@ WSADATA wsadata;
 
 int main(int argc, char *argv[]) 
 {
-    //*******************************************************************
     // Initialization
-    //*******************************************************************
     struct sockaddr_in remoteaddr;
     struct hostent *h;
-
     SOCKET s;
     char send_buffer[BUFFESIZE], receive_buffer[BUFFESIZE];
     int n, bytes;
     memset(&remoteaddr, 0, sizeof(remoteaddr)); //clean up 
-    //*******************************************************************
+
     //WSASTARTUP 
-    //*******************************************************************
-    if (WSAStartup(WSVERS, &wsadata) != 0) {
+    if (WSAStartup(WSVERS, &wsadata) != 0) 
+    {
         WSACleanup();
         printf("WSAStartup failed\n");
         exit(1);
     }
 
-    if (argc != 3) {
+    if (argc != 3) 
+    {
         printf("USAGE: client IP-address port\n");
         exit(1);
     }
-    else {
-        remoteaddr.sin_addr.s_addr = inet_addr(argv[1]);//IP address
+    else 
+    {
+        remoteaddr.sin_addr.s_addr = inet_addr(argv[1]);    //IP address
         remoteaddr.sin_port = htons((u_short)atoi(argv[2]));//Port number
     }
-    //*******************************************************************
+
     //CREATE CLIENT'S SOCKET 
-    //*******************************************************************
     s = socket(AF_INET, SOCK_STREAM, 0);
-    if (s < 0) {
+    if (s < 0) 
+    {
         printf("socket failed\n");
         exit(1);
     }
@@ -59,15 +58,9 @@ int main(int argc, char *argv[])
     //*******************************************************************
     memset(&send_buffer, 0, BUFFESIZE);
     fgets(send_buffer, SEGMENTSIZE, stdin);
-    while (strncmp(send_buffer, ".", 1) != 0) {
-#if defined __unix__ || defined __APPLE__
+    while (strncmp(send_buffer, ".", 1) != 0) 
+    {
         send_buffer[strlen(send_buffer) - 1] = '\0';//strip send_buffer from '\n'
-        printf("lenght is %d \n", (int)strlen(send_buffer));
-#elif defined _WIN32 
-        send_buffer[strlen(send_buffer) - 1] = '\0';//strip send_buffer from '\n'
-        //nothing to do
-#endif
-        printf(">>> %s\n", send_buffer);//line sent
         strcat(send_buffer, "\r\n");
         //*******************************************************************
         //SEND
