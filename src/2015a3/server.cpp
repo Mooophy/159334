0bc115aaa5ced6fb5151e15a523710d142cd465a
Mooophy <moophyalan@gmail.com>
2015-05-27 21:16:09 +1200

    new file:   2015a3/client.cpp 	new file:   2015a3/server.cpp

--- src/2015a3/server.cpp
+++ src/2015a3/server.cpp
@@ -20,19 +20,15 @@ int main(int argc, char *argv[])
 
     //INITIALIZATION
     struct sockaddr_in localaddr, remoteaddr;
-
     SOCKET s, ns;
-
     char send_buffer[BUFFESIZE], receive_buffer[BUFFESIZE];
     memset(&send_buffer, 0, BUFFESIZE);
     memset(&receive_buffer, 0, BUFFESIZE);
-
     int n, bytes, addrlen;
     memset(&localaddr, 0, sizeof(localaddr));//clean up the structure
     memset(&remoteaddr, 0, sizeof(remoteaddr));//clean up the structure
-
     //WSSTARTUP
-    if (WSAStartup(WSVERS, &wsadata) != 0)
+    if (0 != WSAStartup(WSVERS, &wsadata))
     {
         WSACleanup();
         printf("WSAStartup failed\n");
@@ -40,18 +36,14 @@ int main(int argc, char *argv[])
 
     //SOCKET
     s = socket(PF_INET, SOCK_STREAM, 0);
-    if (s < 0)
-    {
-        printf("socket failed\n");
-    }
-
+    if (s < 0) printf("socket failed\n");
     localaddr.sin_family = AF_INET;
     if (argc == 2) localaddr.sin_port = htons((u_short)atoi(argv[1]));
     else localaddr.sin_port = htons(1234);//default listening port
     localaddr.sin_addr.s_addr = INADDR_ANY;//server IP address should be local
 
     //BIND
-    if (bind(s, (struct sockaddr *)(&localaddr), sizeof(localaddr)) != 0)
+    if (0 != bind(s, (struct sockaddr *)(&localaddr), sizeof(localaddr)))
     {
         printf("Bind failed!\n");
         exit(0);
@@ -59,29 +51,18 @@ int main(int argc, char *argv[])
 
     //LISTEN
     listen(s, 5);
-
     while (1)
     {
         addrlen = sizeof(remoteaddr);
-
-        //NEW SOCKET newsocket = accept
         ns = accept(s, (struct sockaddr *)(&remoteaddr), &addrlen);
-
         if (ns < 0) break;
         printf("accepted a connection from client IP %s port %d \n", inet_ntoa(remoteaddr.sin_addr), ntohs(localaddr.sin_port));
 
-        /**
-          * @brief  send public key.
-          */
-        if (Key_counter>2)
-        {
-            Key_counter = 0;
-        }
+        //send public key.
+        if (Key_counter>2) Key_counter = 0;
         sprintf(send_buffer, "Public_Key %d %d\r\n", Keys[Key_counter][0], Keys[Key_counter][1]);
-
         bytes = send(ns, send_buffer, strlen(send_buffer), 0);
-        printf("@alan: %s", send_buffer);
-
+        printf("sending: %s", send_buffer);
         while (1)
         {
             n = 0;
@@ -104,20 +85,12 @@ int main(int argc, char *argv[])
             printf("The client is sending: %s\n", receive_buffer);
             memset(&send_buffer, 0, BUFFESIZE);
             sprintf(send_buffer, "<<< SERVER SAYS:The client typed '%s' - There are %d bytes of information\r\n", receive_buffer, n);
-
-
-
-
             /**
               * @brief  decrypt and save it in the receive_buffer
               */
             decryptor(receive_buffer, Keys[Key_counter][0], Keys[Key_counter][2]);
-            printf("@alan the decrpted message is:'%s'\n", receive_buffer);
-            printf("...................................\n");
-
-
-
-
+            printf("the decrpted message is:'%s'\n...................................\n", receive_buffer);
+            //printf("");
             //SEND
             bytes = send(ns, send_buffer, strlen(send_buffer), 0);
             if (bytes < 0) break;
@@ -125,8 +98,6 @@ int main(int argc, char *argv[])
         //CLOSE SOCKET
         closesocket(ns);//close connecting socket
         printf("disconnected from %s\n", inet_ntoa(remoteaddr.sin_addr));
-
-
         /**
           * @brief  next key
           */
