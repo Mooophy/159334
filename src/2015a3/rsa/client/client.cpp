c01a06ec05e0763083205410aa64ffccaabb6f79
moophy <MoophyAlan@gmail.com>
2015-05-28 12:47:27 +1200

    modified:   client/client.cpp

--- src/2015a3/rsa/client/client.cpp
+++ src/2015a3/rsa/client/client.cpp
@@ -5,59 +5,62 @@ using std::string;
 #include <iostream>
 using std::cout; using std::endl; using std::cin;
 
-const int WSVERS = MAKEWORD(2, 0);
-const int BUFFESIZE = 200;
-
-auto make_remote_address(char* arr[]) -> struct sockaddr_in
+namespace rsa
 {
-    struct sockaddr_in address;
-    memset(&address, 0, sizeof(address));
-    address.sin_addr.s_addr = inet_addr(arr[1]);    //IP address
-    address.sin_port = htons((u_short)atoi(arr[2]));//Port number
-    address.sin_family = AF_INET;
-    return address;
-}
+    const int WSVERS = MAKEWORD(2, 0);
+    const int BUFFESIZE = 200;
 
-auto handle_user_input(int arguments_count) -> void
-{
-    if (arguments_count != 3)
+    auto make_remote_address(char* arr[]) -> struct sockaddr_in
     {
-        cout << "USAGE: client IP-address port" << endl;
-        exit(1);
+        struct sockaddr_in address;
+        memset(&address, 0, sizeof(address));
+        address.sin_addr.s_addr = inet_addr(arr[1]);    //IP address
+        address.sin_port = htons((u_short)atoi(arr[2]));//Port number
+        address.sin_family = AF_INET;
+        return address;
     }
-}
 
-auto setup_win_sock_api(WORD version_required) -> WSADATA
-{
-    WSADATA wsadata;
-    if (WSAStartup(WSVERS, &wsadata) != 0)
+    auto handle_user_input(int arguments_count) -> void
     {
-        WSACleanup();
-        cout << "WSAStartup failed\n";
-        exit(1);
+        if (arguments_count != 3)
+        {
+            cout << "USAGE: client IP-address port" << endl;
+            exit(1);
+        }
     }
-    return wsadata;
-}
 
-//return the string received
-auto receive(SOCKET s) -> string
-{
-    auto received = string();
-    //receive char by char, end on an LF, ignore CR's
-    for (auto ch = char(0); true; /* */)
+    auto setup_win_sock_api(WORD version_required) -> WSADATA
     {
-        if (0 >= recv(s, &ch, 1, 0)) { cout << "recv failed\n"; exit(1); }
-        if (ch == '\n') break; else if (ch == '\r') continue; else received.push_back(ch);
+        WSADATA wsadata;
+        if (WSAStartup(WSVERS, &wsadata) != 0)
+        {
+            WSACleanup();
+            cout << "WSAStartup failed\n";
+            exit(1);
+        }
+        return wsadata;
     }
-    return received;
-}
+
+    //return the string received
+    auto receive(SOCKET s) -> string
+    {
+        auto received = string();
+        //receive char by char, end on an LF, ignore CR's
+        for (auto ch = char(0); true; /* */)
+        {
+            if (0 >= recv(s, &ch, 1, 0)) { cout << "recv failed\n"; exit(1); }
+            if (ch == '\n') break; else if (ch == '\r') continue; else received.push_back(ch);
+        }
+        return received;
+    }
+}//namespace rsa
 
 
 auto main(int argc, char *argv[]) -> int
 {
-    handle_user_input(argc);
-    auto wsa_data = setup_win_sock_api(WSVERS);
-    auto remoteaddr = make_remote_address(argv);
+    rsa::handle_user_input(argc);
+    auto wsa_data = rsa::setup_win_sock_api(rsa::WSVERS);
+    auto remoteaddr = rsa::make_remote_address(argv);
 
     //CREATE CLIENT'S SOCKET 
     auto s = socket(AF_INET, SOCK_STREAM, 0);
@@ -83,7 +86,7 @@ auto main(int argc, char *argv[]) -> int
         if (bytes_sent < 0) { cout << "send failed\n"; exit(1); }
 
         //receive
-        cout << receive(s) << endl;
+        cout << rsa::receive(s) << endl;
     }
 
     closesocket(s);
