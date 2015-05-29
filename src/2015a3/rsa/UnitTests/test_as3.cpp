d8098b8e4353eaaff8a2b017202d316aa013dbd5
moophy <MoophyAlan@gmail.com>
2015-05-29 18:41:56 +1200

    modified:   2015a3/rsa/UnitTests/test_as3.cpp 	modified:   2015a3/rsa/lib/as3.hpp

--- src/2015a3/rsa/UnitTests/test_as3.cpp
+++ src/2015a3/rsa/UnitTests/test_as3.cpp
@@ -5,18 +5,27 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTests
-{		
-	TEST_CLASS(Rsa)
-	{
-	public:
-		
-		TEST_METHOD(key_ctor)
-		{
-            auto key = as3::rsa::Key{ 1, 2, 3 };
-            Assert::AreEqual(1, key.n);
-            Assert::AreEqual(2, key.e);
-            Assert::AreEqual(3, key.d);
-		}
+{
+    TEST_CLASS(Rsa)
+    {
+    public:
 
-	};
+        TEST_METHOD(key_ctor)
+        {
+            auto rkey = as3::rsa::RsaKey{ 1, 2, 3 };
+            Assert::AreEqual((char)1, rkey.n);
+            Assert::AreEqual((char)2, rkey.e);
+            Assert::AreEqual((char)3, rkey.d);
+        }
+
+        TEST_METHOD(RsaKeyList)
+        {
+            using RK = as3::rsa::RsaKey;
+            auto keys = { RK{ 143, 7, 103 }, RK{ 187, 27, 83 }, RK{ 209, 17, 53 } };
+            auto key_list = as3::rsa::RsaKeyList(keys.begin(), keys.end());
+
+            Assert::AreEqual(3u, key_list.data().size());
+        }
+
+    };
 }
--- src/2015a3/rsa/lib/as3.hpp
+++ src/2015a3/rsa/lib/as3.hpp
@@ -5,6 +5,10 @@
 using std::string;
 #include <iostream>
 using std::cout; using std::endl; using std::cin; using std::ostream;
+#include <vector>
+using std::vector;
+#include <tuple>
+using std::make_tuple;
 
 
 namespace as3
@@ -70,14 +74,70 @@ namespace as3
     auto inline send(SOCKET sock, string const& send_buffer) -> void
     {
         auto bytes_sent = ::send(sock, send_buffer.c_str(), send_buffer.size(), 0);
-        if (bytes_sent < 0) { println( "send failed" ); exit(1); }
+        if (bytes_sent < 0) { println("send failed"); exit(1); }
     }
 
     namespace rsa
     {
-        struct Key
+        template<typename Integer>
+        auto repeat_square(Integer x, Integer e, Integer n) -> Integer //(x ^ e) % n.
         {
-            int const n, e, d;
+            auto y = Integer{ 1 };
+            while (e > 0)
+            {
+                if ((e % 2) == 0){ x = (x*x) % n; e /= 2; }
+                else{ y = (x*y) % n; --e; }
+            }
+            return y; 
+        }
+
+        struct RsaKey
+        {
+            char const n, e, d;
+        };
+
+        struct PubKey
+        {
+            char const n, e;
+            auto operator()(char const m) const -> char{ return repeat_square(m, e, n); }
+        };
+        struct PrvKey
+        {
+            char const n, d;
+            auto operator()(char const m) const -> char{ return repeat_square(m, d, n); }
+        };
+
+        class RsaKeyList
+        {
+        public:
+            template<typename Iterator>
+            RsaKeyList(Iterator first, Iterator last)
+                : list_(first, last), curr_{ list_.cbegin() }
+            {}
+
+            auto next() const -> void
+            {
+                if (++curr_ == list_.cend()) curr_ = list_.cbegin();
+            };
+
+            auto current_public_key() const -> PrvKey
+            {
+                return{ curr_->n, curr_->e };
+            }
+
+            auto current_private_key() const -> PubKey
+            {
+                return{ curr_->n, curr_->d };
+            }
+
+            auto data() const -> vector<RsaKey> const&
+            {
+                return list_;
+            }
+
+        private:
+            vector<RsaKey> const list_;
+            mutable vector<RsaKey>::const_iterator curr_;
         };
     }
 
